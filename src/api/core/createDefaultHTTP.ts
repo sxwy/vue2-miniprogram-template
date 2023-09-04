2ea0e96a736c16e48becfdca651c7f2bbc02f654
chenjie <chenjie@szy.cn>
2023-09-04 16:05:59 +0800

    feat: http 代码优化

--- src/api/core/createDefaultHTTP.ts
+++ src/api/core/createDefaultHTTP.ts
@@ -1,11 +1,5 @@
 import http, { HTTP } from './http'
 import { HTTPConfig } from './type'
-import { isSessionExpiredError } from './error'
-import store from '@/store'
-import { handleLoginInvalid } from '@/utils'
-
-let isRefreshToken = false
-let httpList: (() => void)[] = []
 
 export default (
   config: HTTPConfig & {
@@ -22,105 +16,7 @@ export default (
   httpInstance.interceptors.response.use(
     async (response) => {
       if (Object.prototype.toString.call(response.data) === '[object Object]') {
-        const keys = Reflect.ownKeys(response.data)
-        if (keys.includes('code') || keys.includes('returncode')) {
-          const code = String(response.data.code || response.data.returncode)
-          if (code === '10000') {
-            return response.data.body
-          } else if (isSessionExpiredError(response.data)) {
-            if (isRefreshToken) {
-              return new Promise((resolve) => {
-                Reflect.deleteProperty(
-                  response.config.header as object,
-                  'Authorization'
-                )
-                httpList.push(() => {
-                  resolve(httpInstance.request(response.config))
-                })
-              })
-            } else {
-              isRefreshToken = true
-              try {
-                await store.dispatch('user/refreshToken')
-              } catch (error) {
-                handleLoginInvalid()
-                httpList = []
-                isRefreshToken = false
-                return Promise.reject(error)
-              }
-              httpList.forEach((cb) => {
-                cb()
-              })
-              httpList = []
-              isRefreshToken = false
-              Reflect.deleteProperty(
-                response.config.header as object,
-                'Authorization'
-              )
-              return httpInstance.request(response.config)
-            }
-          } else {
-            return Promise.reject(
-              http.createError(
-                code,
-                response.data.message,
-                response.config,
-                response
-              )
-            )
-          }
-        } else if (keys.includes('flag')) {
-          const flag = response.data.flag
-          if (flag) {
-            return response.data.data
-          } else {
-            return Promise.reject(
-              http.createError(
-                response.config.errorCode?.ERROR || '',
-                response.data.msg,
-                response.config,
-                response
-              )
-            )
-          }
-        } else if (keys.includes('success')) {
-          const success = response.data.success
-          if (success) {
-            return response.data.data
-          } else {
-            return Promise.reject(
-              http.createError(
-                response.data.errCode,
-                response.data.errMessage,
-                response.config,
-                response
-              )
-            )
-          }
-        } else if (keys.includes('resultCode')) {
-          const resultCode = response.data.resultCode
-          if (resultCode === 0) {
-            return response.data
-          } else {
-            return Promise.reject(
-              http.createError(
-                resultCode,
-                response.data.error,
-                response.config,
-                response
-              )
-            )
-          }
-        } else {
-          return Promise.reject(
-            http.createError(
-              response.config.errorCode?.ERROR || '',
-              response.config.errorMessage?.ERROR || '',
-              response.config,
-              response
-            )
-          )
-        }
+        return response
       } else {
         return Promise.reject(
           http.createError(
--- src/api/core/createSXWYHTTP.ts
+++ src/api/core/createSXWYHTTP.ts
@@ -1,8 +1,12 @@
+import http from './http'
 import createDefaultHTTP from './createDefaultHTTP'
 import { HTTPConfig } from './type'
-import { isAuthorityError } from './error'
+import { isAuthorityError, isSessionExpiredError } from './error'
 import store from '@/store'
 import { handleLoginInvalid } from '@/utils'
+
+let isRefreshToken = false
+let httpList: (() => void)[] = []
 
 export default (defaultConfig: HTTPConfig) => {
   const defaultHTTP = createDefaultHTTP({ showError: false, ...defaultConfig })
@@ -22,8 +26,117 @@ export default (defaultConfig: HTTPConfig) => {
   )
 
   defaultHTTP.interceptors.response.use(
-    (response) => {
-      return response
+    async (response) => {
+      if (Object.prototype.toString.call(response.data) === '[object Object]') {
+        const keys = Reflect.ownKeys(response.data)
+        if (keys.includes('code') || keys.includes('returncode')) {
+          const code = String(response.data.code || response.data.returncode)
+          if (code === '10000') {
+            return response.data.body
+          } else if (isSessionExpiredError(response.data)) {
+            if (isRefreshToken) {
+              return new Promise((resolve) => {
+                Reflect.deleteProperty(
+                  response.config.header as object,
+                  'Authorization'
+                )
+                httpList.push(() => {
+                  resolve(defaultHTTP.request(response.config))
+                })
+              })
+            } else {
+              isRefreshToken = true
+              try {
+                await store.dispatch('user/refreshToken')
+              } catch (error) {
+                handleLoginInvalid()
+                httpList = []
+                isRefreshToken = false
+                return Promise.reject(error)
+              }
+              httpList.forEach((cb) => {
+                cb()
+              })
+              httpList = []
+              isRefreshToken = false
+              Reflect.deleteProperty(
+                response.config.header as object,
+                'Authorization'
+              )
+              return defaultHTTP.request(response.config)
+            }
+          } else {
+            return Promise.reject(
+              http.createError(
+                code,
+                response.data.message,
+                response.config,
+                response
+              )
+            )
+          }
+        } else if (keys.includes('flag')) {
+          const flag = response.data.flag
+          if (flag) {
+            return response.data.data
+          } else {
+            return Promise.reject(
+              http.createError(
+                response.config.errorCode?.ERROR || '',
+                response.data.msg,
+                response.config,
+                response
+              )
+            )
+          }
+        } else if (keys.includes('success')) {
+          const success = response.data.success
+          if (success) {
+            return response.data.data
+          } else {
+            return Promise.reject(
+              http.createError(
+                response.data.errCode,
+                response.data.errMessage,
+                response.config,
+                response
+              )
+            )
+          }
+        } else if (keys.includes('resultCode')) {
+          const resultCode = response.data.resultCode
+          if (resultCode === 0) {
+            return response.data
+          } else {
+            return Promise.reject(
+              http.createError(
+                resultCode,
+                response.data.error,
+                response.config,
+                response
+              )
+            )
+          }
+        } else {
+          return Promise.reject(
+            http.createError(
+              response.config.errorCode?.ERROR || '',
+              response.config.errorMessage?.ERROR || '',
+              response.config,
+              response
+            )
+          )
+        }
+      } else {
+        return Promise.reject(
+          http.createError(
+            response.config.errorCode?.ERROR || '',
+            response.config.errorMessage?.ERROR || '',
+            response.config,
+            response
+          )
+        )
+      }
     },
     (error: any) => {
       if (isAuthorityError(error)) {
